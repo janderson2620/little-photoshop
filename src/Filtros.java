50206aa9935c0a94ffa5835288f602ff40ed85c6
Janderson Junior <jandersondejuni123@gmail.com>
2023-10-22 13:28:08 -0300

    feitas algumas alterações e adicionado um filtro

--- src/Filtros.java
+++ src/Filtros.java
@@ -7,6 +7,10 @@ import java.io.IOException;
 
 
 public class Filtros {
+
+// =========================================================================================================================
+
+
     public static BufferedImage suavizar(BufferedImage entrada) throws IOException {
         int largura = entrada.getWidth();
         int altura = entrada.getHeight();
@@ -62,6 +66,9 @@ public class Filtros {
 
     }
 
+    // =========================================================================================================================
+
+
     public static BufferedImage realceBorda(BufferedImage entrada) throws IOException {
         int largura = entrada.getWidth();
         int altura = entrada.getHeight();
@@ -112,6 +119,10 @@ public class Filtros {
 
         return saida;
     }
+
+    // =========================================================================================================================
+
+
     public static BufferedImage binarizarImagem(BufferedImage imagem, int limiar) throws IOException {
 
             int largura = imagem.getWidth();
@@ -137,6 +148,9 @@ public class Filtros {
         }
             return novaImagem;
     }
+
+    // =========================================================================================================================
+
 
     public static BufferedImage inverterCores(BufferedImage imagem) throws IOException {
 
@@ -165,75 +179,60 @@ public class Filtros {
         return novaImagem;
     }
 
-        public static BufferedImage filtros(BufferedImage imagem, double[] filtro) throws IOException {
+    // =========================================================================================================================
+
+
+    public static BufferedImage filtros(BufferedImage imagem, double[] filtro) throws IOException {
 
             int largura = imagem.getWidth();
             int altura = imagem.getHeight();
 
+        BufferedImage novaImagem = new BufferedImage(largura, altura, BufferedImage.TYPE_INT_RGB);
 
-            BufferedImage novaImagem = new BufferedImage(largura, altura, BufferedImage.TYPE_INT_RGB);
+        for (int linha = 1; linha < largura - 1; linha++) {
+            for (int coluna = 1; coluna < altura - 1; coluna++) {
 
+                int px1 = imagem.getRGB(linha - 1, coluna - 1);
+                int px2 = imagem.getRGB(linha - 1, coluna);
+                int px3 = imagem.getRGB(linha - 1, coluna + 1);
+                int px4 = imagem.getRGB(linha, coluna - 1);
+                int px5 = imagem.getRGB(linha, coluna);
+                int px6 = imagem.getRGB(linha, coluna + 1);
+                int px7 = imagem.getRGB(linha + 1, coluna - 1);
+                int px8 = imagem.getRGB(linha + 1, coluna);
+                int px9 = imagem.getRGB(linha + 1, coluna + 1);
 
-            for (int linha = 1; linha < largura - 1; linha++) {
-                for (int coluna = 1; coluna < altura - 1; coluna++) {
+                Color cor1 = new Color(px1);
+                Color cor2 = new Color(px2);
+                Color cor3 = new Color(px3);
+                Color cor4 = new Color(px4);
+                Color cor5 = new Color(px5);
+                Color cor6 = new Color(px6);
+                Color cor7 = new Color(px7);
+                Color cor8 = new Color(px8);
+                Color cor9 = new Color(px9);
 
-                    int pixel1 = imagem.getRGB(linha - 1, coluna - 1);
-                    Color cor1 = new Color(pixel1);
-                    int px1 = cor1.getRed();
+                double pixel = (
+                                filtro[0] * cor1.getRed() +
+                                filtro[1] * cor2.getRed() +
+                                filtro[2] * cor3.getRed() +
+                                filtro[3] * cor4.getRed() +
+                                filtro[4] * cor5.getRed() +
+                                filtro[5] * cor6.getRed() +
+                                filtro[6] * cor7.getRed() +
+                                filtro[7] * cor8.getRed() +
+                                filtro[8] * cor9.getRed()
+                );
 
-                    int pixel2 = imagem.getRGB(linha - 1, coluna);
-                    Color cor2 = new Color(pixel2);
-                    int px2 = cor2.getRed();
+                if (pixel > 255) {
+                    pixel = 255;
+                } else if (pixel < 0){
+                    pixel = 0;
+                }
 
-                    int pixel3 = imagem.getRGB(linha - 1, coluna + 1);
-                    Color cor3 = new Color(pixel3);
-                    int px3 = cor3.getRed();
+                Color novaCor = new Color((int) pixel, (int) pixel, (int) pixel);
 
-                    int pixel4 = imagem.getRGB(linha, coluna - 1);
-                    Color cor4 = new Color(pixel4);
-                    int px4 = cor4.getRed();
-
-                    int pixel5 = imagem.getRGB(linha, coluna);
-                    Color cor5 = new Color(pixel5);
-                    int px5 = cor5.getRed();
-
-
-
-
-                    int px6 = imagem.getRGB(linha, coluna + 1);
-                    int px7 = imagem.getRGB(linha + 1, coluna - 1);
-                    int px8 = imagem.getRGB(linha + 1, coluna);
-                    int px9 = imagem.getRGB(linha + 1, coluna + 1);
-
-                    
-                    Color cor6 = new Color(px6);
-                    Color cor7 = new Color(px7);
-                    Color cor8 = new Color(px8);
-                    Color cor9 = new Color(px9);
-
-                    double pixel = 0.0;
-
-                   pixel += (px1 * filtro[0]);
-                   pixel += (px2 * filtro[1]);
-                   pixel += (px3 * filtro[2]);
-                   pixel += (px4 * filtro[3]);
-                   pixel += (px5 * filtro[4]);
-                   pixel += (px6 * filtro[5]);
-                   pixel += (px7 * filtro[6]);
-                   pixel += (px8 * filtro[7]);
-                   pixel += (px9 * filtro[8]);
-
-                    if (pixel > 255) {
-                        pixel = 255;
-                    } else if (pixel < 0) {
-                        pixel = 0;
-                    }
-
-                    int novoValor = (int) pixel;
-                    Color novaCor = new Color(novoValor, novoValor, novoValor);
-
-                    novaImagem.setRGB(linha, coluna, novaCor.getRGB());
-
+                novaImagem.setRGB(linha, coluna, novaCor.getRGB());
 
             }
         }
--- src/Main.java
+++ src/Main.java
@@ -10,44 +10,53 @@ public class Main {
 
     private JFrame frame;
     private JLabel imageLabel;
-    private JButton carregarImagem;
+    private  JButton carregarImagem;
     private JButton realceDeBordaBotao;
     private JButton binarizar;
-    private JButton inverterImagem;
+    private JButton inverterCores;
     private JButton suavilizar;
     private JButton filtro1;
     private JButton filtro2;
     private JButton filtro3;
     private JButton filtro4;
     private JButton filtro5;
-    private JButton filtro6;
-    private JButton filtro7;
-    private JButton filtro8;
+
     private BufferedImage imagem;
 
     public Main() {
         frame = new JFrame("Filtros de Imagem");
-        frame.setSize(800, 600);
+        frame.setSize(1400, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-
         imageLabel = new JLabel();
-        JScrollPane scrollPane = new JScrollPane(imageLabel);
-        frame.add(scrollPane, BorderLayout.CENTER);
 
+        JScrollPane scrollPane = new JScrollPane(imageLabel);
+
+        frame.add(scrollPane, BorderLayout.CENTER);
         JPanel barraDeBotoes = new JPanel();
+
+
         carregarImagem = new JButton("Carregar Imagem");
         realceDeBordaBotao = new JButton("Realce de Borda");
         binarizar = new JButton("Binarizar Imagem");
         suavilizar = new JButton("Suavilizar");
-        filtro1 = new JButton("Filtro1");
-
+        inverterCores = new JButton("Inverter cores");
+        filtro1 = new JButton("Filtro 1");
+        filtro2 = new JButton("Filtro 2");
+        filtro3 = new JButton("Filtro 3");
+        filtro4 = new JButton("Filtro 4");
+        filtro5 = new JButton("Filtro 5");
 
         barraDeBotoes.add(carregarImagem);
         barraDeBotoes.add(realceDeBordaBotao);
         barraDeBotoes.add(suavilizar);
         barraDeBotoes.add(binarizar);
+        barraDeBotoes.add(inverterCores);
         barraDeBotoes.add(filtro1);
+        barraDeBotoes.add(filtro2);
+        barraDeBotoes.add(filtro3);
+        barraDeBotoes.add(filtro4);
+        barraDeBotoes.add(filtro5);
         frame.add(barraDeBotoes, BorderLayout.SOUTH);
 
         carregarImagem.addActionListener(new ActionListener() {
@@ -72,9 +81,9 @@ public class Main {
             public void actionPerformed(ActionEvent e) {
                 if (imagem != null) {
                     BufferedImage imagemSaida = null;
-                    int limimar = 128;
+                    int limiar = 128;
                     try{
-                        imagemSaida = Filtros.binarizarImagem(imagem, limimar);
+                        imagemSaida = Filtros.binarizarImagem(imagem, limiar);
                     }catch (IOException ex){
                         ex.printStackTrace();
                     }
@@ -113,21 +122,113 @@ public class Main {
             }
         });
 
-
-
-        realceDeBordaBotao.addActionListener(new ActionListener() {
+        inverterCores.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (imagem != null) {
                     BufferedImage imagemSaida = null;
+                    try {
+                        imagemSaida = Filtros.inverterCores(imagem);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    exibirImagem(imagemSaida);
+                }
+            }
+        });
+
+        filtro1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (imagem != null) {
+                    BufferedImage imagemSaida = null;
+
                     double[] filter1 = {
-                            0.0625, 0.125, 0.0625,
-                            0.125, 0.25, 0.125,
-                            0.0625, 0.125, 0.0625
+                            1, 1, 1, 0, 0, 0, -1, -1, -1
                     };
 
                     try {
                         imagemSaida = Filtros.filtros(imagem, filter1);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    exibirImagem(imagemSaida);
+                }
+            }
+        });
+
+        filtro2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (imagem != null) {
+                    BufferedImage imagemSaida = null;
+
+                    double[] filter2 = {
+                            1, 0, -1, 1, 0, -1, 1, 0, -1
+                    };
+
+                    try {
+                        imagemSaida = Filtros.filtros(imagem, filter2);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    exibirImagem(imagemSaida);
+                }
+            }
+        });
+
+        filtro3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (imagem != null) {
+                    BufferedImage imagemSaida = null;
+
+                    double[] filter3 = {
+                            -1, -1, -1, -1, 8, -1, -1, -1, -1
+                    };
+
+                    try {
+                        imagemSaida = Filtros.filtros(imagem, filter3);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    exibirImagem(imagemSaida);
+                }
+            }
+        });
+
+        filtro4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (imagem != null) {
+                    BufferedImage imagemSaida = null;
+
+                    double[] filter4 = {
+                            0, -1, 0, -1, 5, -1, 0, -1, 0
+                    };
+
+                    try {
+                        imagemSaida = Filtros.filtros(imagem, filter4);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    exibirImagem(imagemSaida);
+                }
+            }
+        });
+
+        filtro5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (imagem != null) {
+                    BufferedImage imagemSaida = null;
+
+                    double[] filter5 = {
+                            0, 0, 0, -1, 1, 0, 0, 0, 0
+                    };
+
+                    try {
+                        imagemSaida = Filtros.filtros(imagem, filter5);
                     } catch (IOException ex) {
                         throw new RuntimeException(ex);
                     }
